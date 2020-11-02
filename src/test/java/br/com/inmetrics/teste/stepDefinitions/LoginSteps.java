d57bc5315e83b367b2fb63f917aad68a0ca78f82
Allan Cleysson <allancleysson@gmail.com>
2020-11-02 19:32:32 -0300

    alterando steps de login para portugues

--- src/test/java/br/com/inmetrics/teste/stepDefinitions/LoginSteps.java
+++ src/test/java/br/com/inmetrics/teste/stepDefinitions/LoginSteps.java
@@ -2,33 +2,33 @@ package br.com.inmetrics.teste.stepDefinitions;
 
 import br.com.inmetrics.test.page.EmpregadosPage;
 import br.com.inmetrics.test.page.LoginPage;
-import io.cucumber.java.en.*;
+import io.cucumber.java.pt.*;
 
 public class LoginSteps {
 	
-	@Given("que o Usuario deseja efetuar login no sistema")
+	@Dado("que o Usuario deseja efetuar login no sistema")
 	public void acessarLogin(){
 		LoginPage.navigateToLogin();
 	}
 	
-	@When("o usuario digitar o {string}")
+	@Quando("o usuario digitar o {string}")
 	public void digitarUsuario(String login){
 		LoginPage.insertUser(login);
 	}
 	
-	@And ("o usuario digitar a {string}")
+	@E ("o usuario digitar a {string}")
 	public void digitarSenha(String senha){
 		LoginPage.insertPassword(senha);
 	}
 	
-	@Then ("ele sera direcionado para a listagem de empregados")
+	@Entao ("ele sera direcionado para a listagem de empregados")
 	public void clicarEntre(){
 		LoginPage.submit();
 		EmpregadosPage.verifyIfLoggedIn();
 	}
 	
 	
-	@Then("o sistema notificará um erro")
+	@Entao("o sistema notificará um erro")
 	public void verificaErroCredenciais(){
 		LoginPage.submit();
 		LoginPage.verifyErrorMessage();
