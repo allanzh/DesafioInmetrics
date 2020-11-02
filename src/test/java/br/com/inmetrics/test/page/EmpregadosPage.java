298c002507dd81a22213ad621392956a49859947
Allan Cleysson <allancleysson@gmail.com>
2020-11-01 22:14:08 -0300

    adicionando ações da página de listagem de empregados

--- src/test/java/br/com/inmetrics/test/page/EmpregadosPage.java
+++ src/test/java/br/com/inmetrics/test/page/EmpregadosPage.java
@@ -1,7 +1,6 @@
 package br.com.inmetrics.test.page;
 
 import org.junit.Assert;
-
 import br.com.inmetrics.test.core.DriverFactory;
 
 public class EmpregadosPage {
@@ -10,4 +9,27 @@ public class EmpregadosPage {
 		Assert.assertTrue(DriverFactory.verifyElementPresent("//a[.='Sair']"));
 	}
 	
+	public static void createNewEmployee() {
+		DriverFactory.click("//a[.='Novo Funcionário']");
+	}
+	
+	public static void editEmployee() {
+		DriverFactory.click("(//a[contains(@href, '/empregados/edit')])[1]");
+	}
+	
+	public static void deleteEmployee() {
+		DriverFactory.click("(//button[@id=\"delete-btn\"])[1]");
+	}
+	
+	public static void searchEmployee(String query) {
+		DriverFactory.setText("//input[@type='search']", query);
+	}
+	
+	public static void nextPageResult() {
+		DriverFactory.click("//a[.='Próxima']");
+	}
+	
+	public static void previousPageResult() {
+		DriverFactory.click("//a[.='Anterior']");
+	}
 }
